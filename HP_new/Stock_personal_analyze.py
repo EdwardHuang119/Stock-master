7ecf5be04717cf2f023efdf14a83d31a834cdc2d
Edward Huang <26125482@qq.com>
2021-11-18 20:38:07 +0800

    股票QSDD分析处理结构话

--- HP_new/Stock_personal_analyze.py
+++ HP_new/Stock_personal_analyze.py
@@ -21,19 +21,24 @@ def ts_name_get(stock_basic,ts_code):
     # name = name_s['name']
     return name
 
+def QSDD_any_zone(con_code_list,filename,start_date,end_date):
+    stock_basic = pro.query('stock_basic', exchange='', list_status='L',
+                            fields='ts_code,name')
+    code_anaylze = pd.DataFrame()
+    for i in range(len(con_code_list)):
+        code_anaylze_per = QSDD_perstock_withname(stock_basic,con_code_list[i],start_date,end_date)
+        # code_anaylze['name'] = code_anaylze.apply(lambda x: ts_name_get(stock_basic, x['ts_code']), axis=1)
+        code_anaylze_per_tail = code_anaylze_per.tail(10)
+        code_anaylze = pd.concat([code_anaylze, code_anaylze_per_tail], ignore_index=False)
+        i=i+1
+    show_func(code_anaylze)
+    filename = filename+'（' + str(Today) + ')'
+    Tocsv(code_anaylze, '', filename)
+    return code_anaylze
 
-# start_date = start_date.strftime('%Y%m%d')
-# print(Today,start_date)
-
-# index = index_basic()
-# Tocsv(index,'','指数合集')
-# show_func(index)
 list_HS300 = ['000300.SH','399300.SZ']
 list_SZ500 = ['000905.SH','399905.SZ']
-# code_zone = index_weight(list_SZ500,'20210730','20210815','20210910')
-# code_zone = index_weight('000905.SH','','20210901','20210917')
-# Tocsv(code_zone,'','上证500指标')
-# show_func(code_zone)
+
 
 if __name__ == "__main__":
     stock_basic = pro.query('stock_basic', exchange='', list_status='L',
